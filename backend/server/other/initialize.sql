3f8718db3545eb1ba542f7654c4a8c6472f6dbc9
David Horvát <horvatda@proton.me>
2024-04-27 13:49:15 +0200

    database init

---
+++ backend/server/other/initialize.sql
@@ -0,0 +1,31 @@
+-- Users
+CREATE TABLE IF NOT EXISTS users (
+    username text,
+    password_hash text,
+
+    PRIMARY KEY (username)
+);
+
+-- Machines
+CREATE TABLE IF NOT EXISTS machines (
+    id SERIAL,
+    username text,
+    machine_type INTEGER,
+    rotors INTEGER[],
+    character_history CHAR[140][2],
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (username) REFERENCES users(username)
+);
+
+-- Rotors
+CREATE TABLE IF NOT EXISTS rotors (
+    id SERIAL,
+    machine_id SERIAL,
+    rotor_type INTEGER,
+    letter_shift INTEGER,
+    rotor_position INTEGER,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (machine_id) REFERENCES machines(id)
+);
