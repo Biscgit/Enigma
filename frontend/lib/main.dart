47352538d89458b5fcaa6722d3a53eeb11e98a4d
David Horvát <horvatda@proton.me>
2024-05-02 16:16:55 +0200

    moved files

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -5,72 +5,94 @@ class PageVariants extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width; // Get the screen width
+    final screenWidth =
+        MediaQuery.of(context).size.width; // Get the screen width
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Center( // Center the first button
+        Center(
+          // Center the first button
           child: SizedBox(
-            width: screenWidth * 0.5, // Set button width to half of the screen width
-            height: 40, // Adjust the height of the buttons
+            width: screenWidth * 0.5,
+            // Set button width to half of the screen width
+            height: 40,
+            // Adjust the height of the buttons
             child: ElevatedButton(
               onPressed: () {},
               style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all<Color>(Colors.blue), // Change button color
+                backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
+                // Change button color
                 shape: WidgetStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0), // Set button border radius
+                    borderRadius:
+                        BorderRadius.circular(20.0), // Set button border radius
                   ),
                 ),
               ),
               child: const Text(
                 'Enigma I',
-                style: TextStyle(fontSize: 14, color: Colors.white), // Adjust the font size and color
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white), // Adjust the font size and color
               ),
             ),
           ),
         ),
         const SizedBox(height: 8), // Add some space between buttons
-        Center( // Center the second button
+        Center(
+          // Center the second button
           child: SizedBox(
-            width: screenWidth * 0.5, // Set button width to half of the screen width
-            height: 40, // Adjust the height of the buttons
+            width: screenWidth * 0.5,
+            // Set button width to half of the screen width
+            height: 40,
+            // Adjust the height of the buttons
             child: ElevatedButton(
               onPressed: () {},
               style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all<Color>(Colors.blue), // Change button color
+                backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
+                // Change button color
                 shape: WidgetStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0), // Set button border radius
+                    borderRadius:
+                        BorderRadius.circular(20.0), // Set button border radius
                   ),
                 ),
               ),
               child: const Text(
                 'Norway Enigma',
-                style: TextStyle(fontSize: 14, color: Colors.white), // Adjust the font size and color
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white), // Adjust the font size and color
               ),
             ),
           ),
         ),
         const SizedBox(height: 8), // Add some space between buttons
-        Center( // Center the third button
+        Center(
+          // Center the third button
           child: SizedBox(
-            width: screenWidth * 0.5, // Set button width to half of the screen width
-            height: 40, // Adjust the height of the buttons
+            width: screenWidth * 0.5,
+            // Set button width to half of the screen width
+            height: 40,
+            // Adjust the height of the buttons
             child: ElevatedButton(
               onPressed: () {},
               style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all<Color>(Colors.blue), // Change button color
+                backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
+                // Change button color
                 shape: WidgetStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0), // Set button border radius
+                    borderRadius:
+                        BorderRadius.circular(20.0), // Set button border radius
                   ),
                 ),
               ),
               child: const Text(
                 'Enigma M3',
-                style: TextStyle(fontSize: 14, color: Colors.white), // Adjust the font size and color
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white), // Adjust the font size and color
               ),
             ),
           ),
