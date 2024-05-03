48d66cc77605bfcca7f47ad3f0cf1aec3d679c0f
Omar Alsabahi <omar.alsabahi@stud.th-deg.de>
2024-05-03 22:25:29 +0200

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -6,63 +6,103 @@ class PageVariants extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width; // Get the screen width
+    final screenWidth =
+        MediaQuery.of(context).size.width; // Get the screen width
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Center(
+          // Center the first button
           child: SizedBox(
-            width: screenWidth * 0.5, // Set button width to half of the screen width
+            width: screenWidth * 0.5,
+            // Set button width to half of the screen width
             height: 40,
+            // Adjust the height of the buttons
             child: ElevatedButton(
               onPressed: () {
                 _launchURL('https://enigmaI.com');
               },
               style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all<Color>(const Color.fromARGB(255, 33, 150, 243)),
+                backgroundColor: WidgetStateProperty.all<Color>(
+                    const Color.fromARGB(255, 33, 150, 243)),
+                // Change button color
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(20.0), // Set button border radius
+                  ),
+                ),
               ),
               child: const Text(
                 'Enigma I',
-                style: TextStyle(fontSize: 14, color: Colors.white), // Adjust the font size and color
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white), // Adjust the font size and color
               ),
             ),
           ),
         ),
-        const SizedBox(height: 15), // Add some space between buttons
+        const SizedBox(height: 8), // Add some space between buttons
         Center(
+          // Center the second button
           child: SizedBox(
-            width: screenWidth * 0.5, // Set button width to half of the screen width
+            width: screenWidth * 0.5,
+            // Set button width to half of the screen width
             height: 40,
+            // Adjust the height of the buttons
             child: ElevatedButton(
               onPressed: () {
                 _launchURL('https://norwayenigma.com');
               },
               style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all<Color>(const Color.fromARGB(255, 33, 150, 243)),
+                backgroundColor: WidgetStateProperty.all<Color>(
+                    const Color.fromARGB(255, 33, 150, 243)),
+                // Change button color
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(20.0), // Set button border radius
+                  ),
+                ),
               ),
               child: const Text(
                 'Norway Enigma',
-                style: TextStyle(fontSize: 14, color: Colors.white), // Adjust the font size and color
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white), // Adjust the font size and color
               ),
             ),
           ),
         ),
-        const SizedBox(height: 15), // Add some space between buttons
+        const SizedBox(height: 8), // Add some space between buttons
         Center(
+          // Center the third button
           child: SizedBox(
-            width: screenWidth * 0.5, // Set button width to half of the screen width
+            width: screenWidth * 0.5,
+            // Set button width to half of the screen width
             height: 40,
+            // Adjust the height of the buttons
             child: ElevatedButton(
               onPressed: () {
                 _launchURL('https://enigmam3.com');
               },
               style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all<Color>(const Color.fromARGB(255, 33, 150, 243)),
+                backgroundColor: WidgetStateProperty.all<Color>(
+                    const Color.fromARGB(255, 33, 150, 243)),
+                // Change button color
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(20.0), // Set button border radius
+                  ),
+                ),
               ),
               child: const Text(
                 'Enigma M3',
-                style: TextStyle(fontSize: 14, color: Colors.white), // Adjust the font size and color
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white), // Adjust the font size and color
               ),
             ),
           ),
@@ -72,9 +112,8 @@ class PageVariants extends StatelessWidget {
   }
 
   Future<void> _launchURL(String url) async {
-    Uri uri = Uri.parse(url);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri);
+    if (await canLaunch(url)) {
+      await launch(url);
     } else {
       throw 'Could not launch $url';
     }
