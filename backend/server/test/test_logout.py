3182fd08da495cf802a6c75c04b26df47ec77947
David Horvát <horvatda@proton.me>
2024-05-03 17:05:03 +0200

    created logout test

---
+++ backend/server/test/test_logout.py
@@ -0,0 +1,35 @@
+from fastapi.testclient import TestClient
+
+from server.app import app
+from server.lib import routes
+
+client = TestClient(app)
+
+
+def test_logout_valid_token():
+    # setup
+    token = "token-12345-xxx"
+    routes.current_auth = {token: "testuser"}
+
+    # test
+    response = client.get(f"/logout", params={"token": token})
+
+    # check
+    assert response.status_code == 200
+    assert response.json() == {"message": "OK"}
+    assert routes.current_auth == {}
+
+
+def test_logout_invalid_token():
+    # setup
+    token = "token-12345-xxx"
+    invalid_token = "token-invalid!"
+    routes.current_auth = {token: "testuser"}
+
+    # test
+    response = client.get(f"/logout", params={"token": invalid_token})
+
+    # check
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Invalid token!"}
+    assert routes.current_auth == {token: "testuser"}
