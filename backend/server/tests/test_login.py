69437d46cd96f3a55b6f3bef7bd3590ea62f25ba
David Horvát <horvatda@proton.me>
2024-05-04 15:05:30 +0200

    fixed async and impl mockers

--- backend/server/tests/test_login.py
+++ backend/server/tests/test_login.py
@@ -1,6 +1,5 @@
-import uuid
-from fastapi.testclient import TestClient
-from unittest.mock import MagicMock
+import pytest
+from httpx import AsyncClient
 
 from server.app import app
 from server.lib import database, models, routes
@@ -40,21 +39,17 @@ def override_get_database():
 
 
 app.dependency_overrides[database.get_database] = override_get_database
-uuid.uuid4 = MagicMock(return_value="uuid-mock-string")
-
-user_token1 = "ff7b6b5c3bb186091b41eb1a1c057d30f70e0b9566d8303c60d17f71b9b51c24"
-user_token2 = "3eb468c87f54151037964eea70a969ee251e7960629a84274c31d5c412b684a6"
 
 
-def test_login_valid_user():
-    uuid.uuid4 = MagicMock(return_value="uuid-mock-string")
-
+@pytest.mark.anyio
+async def test_login_valid_user(mocked_uuid):
     # setup
     user = models.LoginForm.model_construct(username="user1", password="password1")
     routes.current_auth = {}
 
     # test
-    response = client.post(f"/login", content=user.model_dump_json())
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.post(f"/login", content=user.model_dump_json())
 
     # check
     assert response.status_code == 200
@@ -62,27 +57,31 @@ def test_login_valid_user():
     assert routes.current_auth == {user_token1: "user1"}
 
 
-def test_login_unknown_user():
+@pytest.mark.anyio
+async def test_login_unknown_user(mocked_uuid):
     # setup
     user = models.LoginForm.model_construct(username="user12345", password="no-pass")
     routes.current_auth = {}
 
     # test
-    response = client.post(f"/login", content=user.model_dump_json())
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.post(f"/login", content=user.model_dump_json())
 
     # check
     assert response.status_code == 401
-    assert response.json() == {"detail": "Invalid token!"}
+    assert response.json() == {"detail": "Invalid username or password"}
     assert routes.current_auth == {}
 
 
-def test_login_invalid_password():
+@pytest.mark.anyio
+async def test_login_invalid_password(mocked_uuid):
     # setup
     user = models.LoginForm.model_construct(username="user1", password="wrong-password")
     routes.current_auth = {"dummy-token": "dummy-user"}
 
     # test
-    response = client.post(f"/login", content=user.model_dump_json())
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.post(f"/login", content=user.model_dump_json())
 
     # check
     assert response.status_code == 401
@@ -90,13 +89,15 @@ def test_login_invalid_password():
     assert routes.current_auth == {"dummy-token": "dummy-user"}
 
 
-def test_login_other_password():
+@pytest.mark.anyio
+async def test_login_other_password(mocked_uuid):
     # setup
     user = models.LoginForm.model_construct(username="user2", password="password1")
     routes.current_auth = {}
 
     # test
-    response = client.post(f"/login", content=user.model_dump_json())
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.post(f"/login", content=user.model_dump_json())
 
     # check
     assert response.status_code == 401
@@ -104,30 +105,34 @@ def test_login_other_password():
     assert routes.current_auth == {}
 
 
-def test_login_already_auth():
+@pytest.mark.anyio
+async def test_login_already_auth(mocked_uuid):
     # setup
     user = models.LoginForm.model_construct(username="user2", password="password2")
-    routes.current_auth = {"demo-token": "user1"}
+    routes.current_auth = {"demo-token": "user2"}
     expected_token = user_token2
 
     # test
-    response = client.post(f"/login", content=user.model_dump_json())
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.post(f"/login", content=user.model_dump_json())
 
     # check
-    assert response.status_code == 401
-    assert response.json() == {"detail": "Invalid username or password"}
+    assert response.status_code == 200
+    assert response.json() == {"token": expected_token}
     assert routes.current_auth == {expected_token: "user2"}
 
 
-def test_different_logins():
+@pytest.mark.anyio
+async def test_different_logins(mocked_uuid):
     # setup
     user1 = models.LoginForm.model_construct(username="user1", password="password1")
     user2 = models.LoginForm.model_construct(username="user2", password="password2")
     routes.current_auth = {}
 
     # test
-    response1 = client.post(f"/login", content=user1.model_dump_json())
-    response2 = client.post(f"/login", content=user2.model_dump_json())
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response1 = await ac.post(f"/login", content=user1.model_dump_json())
+        response2 = await ac.post(f"/login", content=user2.model_dump_json())
 
     # check
     assert response1.status_code == 200
