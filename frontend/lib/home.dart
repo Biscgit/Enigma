e74941a8333277c7a8cd13bc4b2c56d99d65bab9
Sam <samuel.backof@stud.th-deg.de>
2024-05-04 17:30:41 +0200

    added additional page

---
+++ frontend/lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Template page
+class TemplatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exmaple'),
+      ),
+      body: Center(
+        child: Text('successful login'),
+      ),
+    );
+  }
+}
