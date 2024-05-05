13cc57f665ade8dce7ad1dc1730b404d030e1fd2
Sam <samuel.backof@stud.th-deg.de>
2024-05-05 13:26:10 +0200

    added logout button

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
+// choose between 3 different enigma machines
+//...
+
+//----------------------------------------------------
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            tooltip: 'Logout',
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, '/login');
+            },
+          ),
+        ],
       ),
       body: Center(
-        child: Text('Works'),
+        child: Text('Choose the enigma machine:'),
       ),
     );
   }
