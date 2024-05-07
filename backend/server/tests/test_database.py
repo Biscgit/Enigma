b1f1787a68f1572b73c1e1cf3a71b36c3a1b039f
David Horvát <horvatda@proton.me>
2024-05-07 19:47:45 +0200

    created first working database test

---
+++ backend/server/tests/test_database.py
@@ -0,0 +1,39 @@
+import asyncio
+import logging
+import os
+
+from testcontainers.postgres import PostgresContainer
+import pytest
+
+from server.lib import database
+
+pytest_plugins = ('pytest_asyncio',)
+
+
+@pytest.mark.asyncio
+async def testcontainer_postgres(monkeypatch):
+    # setup
+    asyncio.get_event_loop()
+
+    with PostgresContainer(
+            "postgres:16-alpine",
+            driver=None
+    ) as pg:
+        # issue with testcontainers: wrong port mapping on postgres
+        port = pg.get_connection_url().split(":")[-1].split('/')[0]
+
+        # set env for db connection
+        monkeypatch.setenv("DB_USER", pg.username)
+        monkeypatch.setenv("DB_PASSWORD", pg.password)
+        monkeypatch.setenv("DB_NAME", pg.dbname)
+        monkeypatch.setenv("DB_PORT", port)
+        monkeypatch.setenv("IP_POSTGRES", pg.get_container_host_ip())
+
+        # launch connection
+        db = database.get_database()
+        logging.info(os.getcwd())
+        await db.connect()
+
+        # ensure one connection
+        with pytest.raises(Exception):
+            await db.connect()
