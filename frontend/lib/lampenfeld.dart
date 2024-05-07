ebb8f7c9c9ad76ab1f75699d52aef01a049c0f85
Thomas Schmelmer <thomas.schmelmer@stud.th-deg.de>
2024-05-07 18:29:33 +0200

    Update lampenfeld.dart

--- frontend/lib/lampenfeld.dart
+++ frontend/lib/lampenfeld.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
-
-/*
-
-- Remove TextField commented code soon
-- Add comments
-- Add tests
-
-*/
-
+import 'package:flutter/services.dart'; 
 
 void main() {
   runApp(Lampfield());
 }
 
 class Lampfield extends StatefulWidget {
-  late LampfieldState lampfieldState; //Access the state from outside if needed (i.e. for sendTextInputToLampfield)
-  
+  late LampfieldState lampfieldState;
+
   @override
   State<Lampfield> createState() {
     lampfieldState = LampfieldState();
@@ -30,7 +22,8 @@ class Lampfield extends StatefulWidget {
 class LampfieldState extends State<Lampfield> {
   int counter = 0;
   final double seizedBoxHeight = 10;
-  final List<GlobalKey<CircularTextBoxState>>listOfGlobalKeys = List.generate(26, (index) => GlobalKey<CircularTextBoxState>());
+  final List<GlobalKey<CircularTextBoxState>> listOfGlobalKeys =
+      List.generate(26, (index) => GlobalKey<CircularTextBoxState>());
 
   String lightUpLetter(String characterToLightUp) {
     //This method converts all inputs to uppercase, looks at only the first character and then subtracts the 65 that are added to every index due to ASCII indeces
@@ -40,22 +33,22 @@ class LampfieldState extends State<Lampfield> {
     int letter = characterToLightUp.toUpperCase().codeUnitAt(0) - 65;
     for (int i = 0; i < 26; i++) {
       setState(() {
-          listOfGlobalKeys[i].currentState?.changeColor(letter == i);
+        listOfGlobalKeys[i].currentState?.changeColor(letter == i);
       });
     }
-    return characterToLightUp; //might be necessary? might also not be necessary, who knows
+    return characterToLightUp;
   }
 
   Future<void> sendTextInputToLampfieldAsync(String input) async {
     //API to send textinputs as async calls
-    for(int i = 0; i < input.length; i++) {
+    for (int i = 0; i < input.length; i++) {
       lightUpLetter(input[i]);
     }
   }
 
   void sendTextInputToLampfield(String input) {
     //API to send textinputs without utilising async calls
-    for(int i = 0; i < input.length; i++) {
+    for (int i = 0; i < input.length; i++) {
       lightUpLetter(input[i]);
     }
   }
@@ -65,65 +58,71 @@ class LampfieldState extends State<Lampfield> {
     return MaterialApp(
       home: Scaffold(
         body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [ //Initialises 26 textboxes that look like lamps in a QWERTZ keyboard layout. Globalkey-index is spot_in_alphabet - 1.
-                  CircularTextBox(key: listOfGlobalKeys[16], text: 'Q'),
-                  CircularTextBox(key: listOfGlobalKeys[22], text: 'W'),
-                  CircularTextBox(key: listOfGlobalKeys[4], text: 'E'),
-                  CircularTextBox(key: listOfGlobalKeys[17], text: 'R'),
-                  CircularTextBox(key: listOfGlobalKeys[19], text: 'T'),
-                  CircularTextBox(key: listOfGlobalKeys[25], text: 'Z'),
-                  CircularTextBox(key: listOfGlobalKeys[20], text: 'U'),
-                  CircularTextBox(key: listOfGlobalKeys[8], text: 'I'),
-                  CircularTextBox(key: listOfGlobalKeys[14], text: 'O'),
-                  CircularTextBox(key: listOfGlobalKeys[15], text: 'P'),
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Initialises 26 textboxes that look like lamps in a QWERTZ keyboard layout. Globalkey-index is spot_in_alphabet - 1.
+                CircularTextBox(key: listOfGlobalKeys[16], text: 'Q'),
+                CircularTextBox(key: listOfGlobalKeys[22], text: 'W'),
+                CircularTextBox(key: listOfGlobalKeys[4], text: 'E'),
+                CircularTextBox(key: listOfGlobalKeys[17], text: 'R'),
+                CircularTextBox(key: listOfGlobalKeys[19], text: 'T'),
+                CircularTextBox(key: listOfGlobalKeys[25], text: 'Z'),
+                CircularTextBox(key: listOfGlobalKeys[20], text: 'U'),
+                CircularTextBox(key: listOfGlobalKeys[8], text: 'I'),
+                CircularTextBox(key: listOfGlobalKeys[14], text: 'O'),
+                CircularTextBox(key: listOfGlobalKeys[15], text: 'P'),
+              ],
+            ),
+            SizedBox(height: seizedBoxHeight),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularTextBox(key: listOfGlobalKeys[0], text: 'A'),
+                CircularTextBox(key: listOfGlobalKeys[18], text: 'S'),
+                CircularTextBox(key: listOfGlobalKeys[3], text: 'D'),
+                CircularTextBox(key: listOfGlobalKeys[5], text: 'F'),
+                CircularTextBox(key: listOfGlobalKeys[6], text: 'G'),
+                CircularTextBox(key: listOfGlobalKeys[7], text: 'H'),
+                CircularTextBox(key: listOfGlobalKeys[9], text: 'J'),
+                CircularTextBox(key: listOfGlobalKeys[10], text: 'K'),
+                CircularTextBox(key: listOfGlobalKeys[11], text: 'L'),
+              ],
+            ),
+            SizedBox(height: seizedBoxHeight),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularTextBox(key: listOfGlobalKeys[24], text: 'Y'),
+                CircularTextBox(key: listOfGlobalKeys[23], text: 'X'),
+                CircularTextBox(key: listOfGlobalKeys[2], text: 'C'),
+                CircularTextBox(key: listOfGlobalKeys[21], text: 'V'),
+                CircularTextBox(key: listOfGlobalKeys[1], text: 'B'),
+                CircularTextBox(key: listOfGlobalKeys[13], text: 'N'),
+                CircularTextBox(key: listOfGlobalKeys[12], text: 'M')
+              ],
+            ),
+            SizedBox(
+              //ONLY FOR MANUAL TEXT INPUTS!!!!! TO BE REMOVED LATER!!
+              width: 300,
+              child: TextField(
+                onChanged: (String value) {
+                  if (value.isNotEmpty) {
+                    lightUpLetter(value.substring(value.length - 1));
+                  } else {
+                    lightUpLetter(
+                        "."); //Special characters dont light up anything; as it should be.
+                  }
+                },
+                inputFormatters: [
+                  FilteringTextInputFormatter.deny(
+                      RegExp(r'\d')), // Not possible anymore to type numbers.
                 ],
               ),
-              SizedBox(height: seizedBoxHeight),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircularTextBox(key: listOfGlobalKeys[0], text: 'A'),
-                  CircularTextBox(key: listOfGlobalKeys[18], text: 'S'),
-                  CircularTextBox(key: listOfGlobalKeys[3], text: 'D'),
-                  CircularTextBox(key: listOfGlobalKeys[5], text: 'F'),
-                  CircularTextBox(key: listOfGlobalKeys[6], text: 'G'),
-                  CircularTextBox(key: listOfGlobalKeys[7], text: 'H'),
-                  CircularTextBox(key: listOfGlobalKeys[9], text: 'J'),
-                  CircularTextBox(key: listOfGlobalKeys[10], text: 'K'),
-                  CircularTextBox(key: listOfGlobalKeys[11], text: 'L'),
-                ],
-              ),
-              SizedBox(height: seizedBoxHeight),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircularTextBox(key: listOfGlobalKeys[24], text: 'Y'),
-                  CircularTextBox(key: listOfGlobalKeys[23], text: 'X'),
-                  CircularTextBox(key: listOfGlobalKeys[2], text: 'C'),
-                  CircularTextBox(key: listOfGlobalKeys[21], text: 'V'),
-                  CircularTextBox(key: listOfGlobalKeys[1], text: 'B'),
-                  CircularTextBox(key: listOfGlobalKeys[13], text: 'N'),
-                  CircularTextBox(key: listOfGlobalKeys[12], text: 'M')
-                ],
-              ),
-              //SizedBox( //ONLY FOR MANUAL TEXT INPUTS!!!!! TO BE REMOVED LATER!!
-                //width: 300,
-                //child: TextField(
-                  //onChanged: (String value) {
-                    //if(value.isNotEmpty) {
-                      //lightUpLetter(value.substring(value.length - 1));
-                    //}
-                    //else {
-                      //lightUpLetter("."); //Special characters dont light up anything; as it should be.
-                    //}
-                  //}
-                //)
-              //)
-            ],
+            ),
+          ],
         ),
       ),
     );
@@ -164,10 +163,9 @@ class CircularTextBoxState extends State<CircularTextBox> {
 
   void changeColor(bool color) {
     setState(() {
-      if(color) {
+      if (color) {
         colorBox = widget.highlightedColor;
-      }
-      else {
+      } else {
         colorBox = widget.defaultColorBox;
       }
     });
