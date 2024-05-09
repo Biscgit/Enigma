5423790bac2906ba951243ebc855d8fdf94b8168
Sam <samuel.backof@stud.th-deg.de>
2024-05-09 14:54:34 +0200

    added login check

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,12 +4,25 @@ import 'home.dart';
 //import 'rotor1.dart';
 //import 'rotor2.dart';
 //import 'rotor3.dart';
+import 'dart:html';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  bool isUserLoggedIn() {
+    String cookies = document.cookie ?? "";
+    Map<String, String> cookieMap = {};
+    cookies.split(";").forEach((String cookie) {
+      List<String> kv = cookie.split("=");
+      if (kv.length == 2) {
+        cookieMap[kv[0].trim()] = kv[1].trim();
+      }
+    });
+    return cookieMap.containsKey("token") && cookieMap["token"]!.isNotEmpty;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,10 +31,18 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => LoginPage(),
-        '/home': (context) => HomePage(),
+      onGenerateRoute: (settings) {
+        if (settings.name == '/home' && !isUserLoggedIn()) {
+          return MaterialPageRoute(builder: (context) => LoginPage());
+        }
+        switch (settings.name) {
+          case '/login':
+            return MaterialPageRoute(builder: (context) => LoginPage());
+          case '/home':
+            return MaterialPageRoute(builder: (context) => HomePage());
+          default:
+            return MaterialPageRoute(builder: (context) => LoginPage());
+        }
       },
     );
   }
