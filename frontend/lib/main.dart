6f3fbea39b172a9f4ff96dc8ff1b6f622db2aea7
Omar Alsabahi <omar.alsabahi@stud.th-deg.de>
2024-05-11 00:53:03 +0200

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,125 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-class PageVariants extends StatelessWidget {
-  const PageVariants({Key? key});
-
-  @override
-  Widget build(BuildContext context) {
-    final screenWidth =
-        MediaQuery.of(context).size.width; // Get the screen width
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          // Center the first button
-          child: SizedBox(
-            width: screenWidth * 0.5,
-            // Set button width to half of the screen width
-            height: 40,
-            // Adjust the height of the buttons
-            child: ElevatedButton(
-              onPressed: () {
-                _launchURL('https://enigmai.com');
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color.fromARGB(255, 33, 150, 243)),
-                // Change button color
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(20.0), // Set button border radius
-                  ),
-                ),
-              ),
-              child: const Text(
-                'Enigma I',
-                style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.white), // Adjust the font size and color
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(height: 8), // Add some space between buttons
-        Center(
-          // Center the second button
-          child: SizedBox(
-            width: screenWidth * 0.5,
-            // Set button width to half of the screen width
-            height: 40,
-            // Adjust the height of the buttons
-            child: ElevatedButton(
-              onPressed: () {
-                _launchURL('https://google.com');
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color.fromARGB(255, 33, 150, 243)),
-                // Change button color
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(20.0), // Set button border radius
-                  ),
-                ),
-              ),
-              child: const Text(
-                'Norway Enigma',
-                style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.white), // Adjust the font size and color
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(height: 8), // Add some space between buttons
-        Center(
-          // Center the third button
-          child: SizedBox(
-            width: screenWidth * 0.5,
-            // Set button width to half of the screen width
-            height: 40,
-            // Adjust the height of the buttons
-            child: ElevatedButton(
-              onPressed: () {
-                _launchURL('https://enigmam3.com');
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color.fromARGB(255, 33, 150, 243)),
-                // Change button color
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(20.0), // Set button border radius
-                  ),
-                ),
-              ),
-              child: const Text(
-                'Enigma M3',
-                style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.white), // Adjust the font size and color
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Future<void> _launchURL(String url) async {
-    Uri uri = Uri.parse(url);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key});
@@ -127,16 +6,154 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue, // Set app bar color to blue
-          title: const Text(
-            'Choose Enigma mode',
-            style: TextStyle(fontSize: 24, color: Colors.white), // Adjust the font size and color
-          ),
-          centerTitle: true, // Center the title horizontally
+      home: const EnigmaPage(),
+    );
+  }
+}
+
+class EnigmaPage extends StatefulWidget {
+  const EnigmaPage({Key? key}) : super(key: key);
+
+  @override
+  _EnigmaPageState createState() => _EnigmaPageState();
+}
+
+class _EnigmaPageState extends State<EnigmaPage> {
+  String currentMachine = 'Enigma I';
+
+  void loadEnigmaI() {
+    setState(() {
+      currentMachine = 'Enigma I';
+    });
+  }
+
+  void loadNorwayEnigma() {
+    setState(() {
+      currentMachine = 'Norway Enigma';
+    });
+  }
+
+  void loadEnigmaM3() {
+    setState(() {
+      currentMachine = 'Enigma M3';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          'Choose Enigma mode',
+          style: TextStyle(fontSize: 24, color: Colors.white),
         ),
-        body: const PageVariants(),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              width: screenWidth * 0.5,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  loadEnigmaI();
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Colors.green; // Change button color if pressed
+                      }
+                      return currentMachine == 'Enigma I'
+                          ? Colors.green // Change button color if selected
+                          : const Color.fromARGB(255, 33, 150, 243);
+                    },
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Enigma I',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Center(
+            child: SizedBox(
+              width: screenWidth * 0.5,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  loadNorwayEnigma();
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Colors.green; // Change button color if pressed
+                      }
+                      return currentMachine == 'Norway Enigma'
+                          ? Colors.green // Change button color if selected
+                          : const Color.fromARGB(255, 33, 150, 243);
+                    },
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Norway Enigma',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Center(
+            child: SizedBox(
+              width: screenWidth * 0.5,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  loadEnigmaM3();
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Colors.green; // Change button color if pressed
+                      }
+                      return currentMachine == 'Enigma M3'
+                          ? Colors.green // Change button color if selected
+                          : const Color.fromARGB(255, 33, 150, 243);
+                    },
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Enigma M3',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
