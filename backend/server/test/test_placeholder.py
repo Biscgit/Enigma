8c6f6494cf4fe3541c895d6ef745a952f7e36f09
David Horvát <horvatda@proton.me>
2024-05-11 09:33:43 +0200

    added temp db tests

--- backend/server/test/test_placeholder.py
+++ backend/server/test/test_placeholder.py
@@ -1,2 +1,29 @@
-def test_function():
-    assert True is not False
+import asyncpg
+from testcontainers.postgres import PostgresContainer
+import pytest
+
+from server.lib import database, models
+
+pytest_plugins = ('pytest_asyncio',)
+
+
+@pytest.mark.asyncio
+async def test_postgres_credentials(monkeypatch):
+    # setup
+    with PostgresContainer(
+            "postgres:16-alpine",
+            driver=None
+    ) as pg:
+        # issue with testcontainers: wrong port mapping on postgres
+        connection_url = pg.get_connection_url()
+        port = connection_url.split(":")[-1].split('/')[0]
+
+        # set env for to be tested db connection
+        monkeypatch.setenv("DB_USER", pg.username)
+        monkeypatch.setenv("DB_PASSWORD", pg.password)
+        monkeypatch.setenv("DB_NAME", pg.dbname)
+        monkeypatch.setenv("DB_PORT", port)
+        monkeypatch.setenv("IP_POSTGRES", pg.get_container_host_ip())
+
+        # prepare database
+        await asyncpg.connect(pg.get_connection_url())
