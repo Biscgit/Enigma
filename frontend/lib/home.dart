45b326a3e8efa47a48618d8dd4e593cea25ee522
ts19084 <thomas.schauer-koeckeis@stud.th-deg.de>
2024-05-12 16:45:09 +0200

    Added changing Title

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -3,15 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:enigma/lampenfeld.dart';
 import 'package:enigma/sidebar.dart';
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-class HomePage extends StatelessWidget {
+class _HomePageState extends State<HomePage> {
+  String selectedItem = 'Enigma I'; 
+
+  void updateSelectedItem(String item) {
+    setState(() {
+      selectedItem = item;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Page'),
+        title: Text('$selectedItem'),
       ),
-      drawer: SideBar(),
+      drawer: SideBar(onItemSelected: updateSelectedItem),
       body: Lampfield(),
     );
   }
--- frontend/lib/sidebar.dart
+++ frontend/lib/sidebar.dart
@@ -1,37 +1,12 @@
 import 'package:flutter/material.dart';
 
-class SideBar extends StatefulWidget {
-//  const SideBar({Key? key}) : super(key: key);
+class SideBar extends StatelessWidget {
+  final Function(String) onItemSelected;
 
-  @override
-  State<SideBar> createState() => SideBarState();
-}
-
-class SideBarState extends State<SideBar> {
-  String currentMachine = 'Enigma I';
-
-  void loadEnigmaI() {
-    setState(() {
-      currentMachine = 'Enigma I';
-    });
-  }
-
-  void loadNorwayEnigma() {
-    setState(() {
-      currentMachine = 'Norway Enigma';
-    });
-  }
-
-  void loadEnigmaM3() {
-    setState(() {
-      currentMachine = 'Enigma M3';
-    });
-  }
+  SideBar({required this.onItemSelected});
 
   @override
   Widget build(BuildContext context) {
- //   final screenWidth = MediaQuery.of(context).size.width;
-
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -45,18 +20,21 @@ class SideBarState extends State<SideBar> {
           ListTile(
             title: Text('Enigma I'),
             onTap: () {
+              onItemSelected('Enigma I');
 	      Navigator.pop(context);
             },
           ),
           ListTile(
             title: Text('Norway Enigma'),
             onTap: () {
+              onItemSelected('Norway Enigma');
 	      Navigator.pop(context);
             },
           ),
           ListTile(
             title: Text('Enigma 3'),
             onTap: () {
+              onItemSelected('Enigma 3');
 	      Navigator.pop(context);
             },
           ),
@@ -65,83 +43,3 @@ class SideBarState extends State<SideBar> {
     );
   }
 }
-
-
-/*
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ,
-          const SizedBox(height: 8),
-          Center(
-            child: SizedBox(
-              width: screenWidth * 0.5,
-              height: 40,
-              child: ElevatedButton(
-                onPressed: () {
-                  loadNorwayEnigma();
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                    (Set<MaterialState> states) {
-                      if (states.contains(MaterialState.pressed)) {
-                        return Colors.green; // Change button color if pressed
-                      }
-                      return currentMachine == 'Norway Enigma'
-                          ? Colors.green // Change button color if selected
-                          : const Color.fromARGB(255, 33, 150, 243);
-                    },
-                  ),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                  ),
-                ),
-                child: const Text(
-                  'Norway Enigma',
-                  style: TextStyle(fontSize: 14, color: Colors.white),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 8),
-          Center(
-            child: SizedBox(
-              width: screenWidth * 0.5,
-              height: 40,
-              child: ElevatedButton(
-                onPressed: () {
-                  loadEnigmaM3();
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                    (Set<MaterialState> states) {
-                      if (states.contains(MaterialState.pressed)) {
-                        return Colors.green; // Change button color if pressed
-                      }
-                      return currentMachine == 'Enigma M3'
-                          ? Colors.green // Change button color if selected
-                          : const Color.fromARGB(255, 33, 150, 243);
-                    },
-                  ),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                  ),
-                ),
-                child: const Text(
-                  'Enigma M3',
-                  style: TextStyle(fontSize: 14, color: Colors.white),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-*/
