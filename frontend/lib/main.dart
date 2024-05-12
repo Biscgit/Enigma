2f6867f7f06d58e484df40b8c23fab37d4e85032
ts19084 <thomas.schauer-koeckeis@stud.th-deg.de>
2024-05-12 15:26:50 +0200

    added routes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,5 +4,27 @@ import 'package:enigma/lampenfeld.dart' as lampenfeld;
 //Other flutter files can be run via flutter run <filepath>
 
 void main() {
-  lampenfeld.main();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Enigma Web App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/login':
+            return MaterialPageRoute(builder: (context) => LoginPage());
+          case '/home':
+            return MaterialPageRoute(builder: (context) => HomePage());
+          default:
+            return MaterialPageRoute(builder: (context) => LoginPage());
+        }
+      },
+    );
+  }
 }
