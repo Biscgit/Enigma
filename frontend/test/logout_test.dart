9eed255362062005c6a04ecc2f077cd65b7af56e
David Horvát <horvatda@proton.me>
2024-05-13 10:22:08 +0200

    added todo

--- frontend/test/logout_test.dart
+++ frontend/test/logout_test.dart
@@ -5,21 +5,24 @@ import 'package:enigma/main.dart' as app;
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-    testWidgets('Successful logout test', (tester) async {
-      app.main();
-      await tester.pumpAndSettle();
+  testWidgets('Successful logout test', (tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+    expect(find.text('Login Page'), findsOneWidget);
 
-      // Navigation to homepage
-      await tester.tap(find.byIcon(Icons.exit_to_app));
-      await tester.pumpAndSettle();
+    // login into app
 
-      // Confirm logout widget
-      expect(find.byType(AlertDialog), findsOneWidget);
-      expect(find.text('Successfully logged out'), findsOneWidget);
-      await tester.tap(find.text('OK'));
-      await tester.pumpAndSettle();
+    // Navigation to homepage
+    await tester.tap(find.byIcon(Icons.exit_to_app));
+    await tester.pumpAndSettle();
 
-      // check if the user is navigated back to login page
-      expect(find.text('Login Page'), findsOneWidget);
-    });
+    // Confirm logout widget
+    expect(find.byType(AlertDialog), findsOneWidget);
+    expect(find.text('Successfully logged out'), findsOneWidget);
+    await tester.tap(find.text('OK'));
+    await tester.pumpAndSettle();
+
+    // check if the user is navigated back to login page
+    expect(find.text('Login Page'), findsOneWidget);
+  });
 }
