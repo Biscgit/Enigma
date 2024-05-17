5a97d4acff8829610ff84f88e1b2c629e3fb6c96
David Horvát <horvatda@proton.me>
2024-05-17 08:52:06 +0200

    added no_stdout option

--- backend/server/lib/logger.py
+++ backend/server/lib/logger.py
@@ -4,16 +4,17 @@ import logging
 import sys
 
 
-def configure_logger():
+def configure_logger(no_stdout=False):
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    stream_handler = logging.StreamHandler(sys.stdout)
-    log_formatter = logging.Formatter(
-        "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
-    )
-    stream_handler.setFormatter(log_formatter)
 
     for h in logger.handlers:
         logger.removeHandler(h)
 
-    logger.addHandler(stream_handler)
+    if not no_stdout:
+        stream_handler = logging.StreamHandler(sys.stdout)
+        log_formatter = logging.Formatter(
+            "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+        )
+        stream_handler.setFormatter(log_formatter)
+        logger.addHandler(stream_handler)
