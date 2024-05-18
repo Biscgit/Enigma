032e1b6cb81ec5f8a123ea09c3404cc792ff08e6
David Horvát <horvatda@proton.me>
2024-05-18 13:20:16 +0200

    created endpoints

---
+++ backend/server/lib/routes/key_input.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends
+from server.lib.database import get_database, Database
+
+from .authentication import check_auth
+
+router = APIRouter()
+
+
+@router.get("/key_press")
+async def encrypt_key(
+        key: str, machine: int,
+        username: str = Depends(check_auth),
+        db_conn: "Database" = Depends(get_database)
+) -> dict:
+    """Endpoint for logging key presses. Takes token, key and machine id. Returns encrypted key"""
+
+    # ToDo: implement encryption here and store it to `encrypted_key`
+    # ToDo: implement check if machine exists
+    encrypted_key: str = "o"
+
+    # save to history and return key
+    await db_conn.save_keyboard_pair(username, machine, key, encrypted_key)
+    return {"key": encrypted_key}
+
+
+@router.get("/load_key_history")
+async def load_key_history(
+        machine: int,
+        username: str = Depends(check_auth),
+        db_conn: "Database" = Depends(get_database)
+) -> list[list[str]]:
+    """Endpoint for loading key history. Takes token and machine id. Returns history of keys pressed"""
+    return await db_conn.get_key_pairs(username, machine)
