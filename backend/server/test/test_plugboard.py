541a201bfc1cc932175eec8391c1623be497bf83
Omar Alsabahi <omar.alsabahi@stud.th-deg.de>
2024-06-02 17:30:09 +0200

    Update test_plugboard.py

--- backend/server/test/test_plugboard.py
+++ backend/server/test/test_plugboard.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI
 from server.lib.routes.plugboard import router
 from server.lib.database import Database, get_database
 from server.lib.routes.authentication import check_auth
@@ -9,6 +9,7 @@ from pydantic import BaseModel
 # Create a TestApp to include the router
 app = FastAPI()
 app.include_router(router)
+
 
 # Mock Database dependency
 class MockDatabase(Database):
@@ -30,20 +31,25 @@ class MockDatabase(Database):
                 if not (key_1 in pair or key_2 in pair)
             ]
 
+
 class PlugboardResponse(BaseModel):
     message: str
+
 
 # Mock authentication dependency
 def override_check_auth(username="test_user"):
     return username
 
+
 # Override dependencies
 app.dependency_overrides[get_database] = MockDatabase
 app.dependency_overrides[check_auth] = override_check_auth
 
+
 @pytest.fixture
 def client():
     return TestClient(app)
+
 
 def test_configure_plugboard(client):
     response = client.post("/plugboard/save?machine=1",
@@ -54,6 +60,7 @@ def test_configure_plugboard(client):
     assert isinstance(data["plugboard"], list)
     assert ["c", "d"] in data["plugboard"]
 
+
 def test_get_configuration(client):
     response = client.get("/plugboard/load", params={"machine": 1})
     assert response.status_code == 200
@@ -61,6 +68,7 @@ def test_get_configuration(client):
     assert "plugboard" in data
     assert isinstance(data["plugboard"], list)
     assert ['a', 'b'] in data["plugboard"]
+
 
 def test_edit_plugboard(client):
     response = client.put("/plugboard/edit?machine=1&letter=a",
@@ -70,12 +78,14 @@ def test_edit_plugboard(client):
     assert "detail" in data
     assert "Letter does not exist in the plugboard" in data["detail"]
 
+
 def test_reset_plugboard(client):
     response = client.delete("/plugboard/reset", params={"machine": 1, "key_1": "a", "key_2": "b"})
     assert response.status_code == 200
     data = response.json()
     assert "message" in data
     assert data["message"] == "Plugboard reset successfully"
+
 
 if __name__ == "__main__":
     pytest.main()
