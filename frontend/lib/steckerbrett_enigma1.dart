9beb99228f2efd40b25540796afaf712d265cd22
David Horvát <horvatda@proton.me>
2024-06-07 13:07:15 +0200

    fixed multiple issues

--- frontend/lib/steckerbrett_enigma1.dart
+++ frontend/lib/steckerbrett_enigma1.dart
@@ -5,12 +5,11 @@ import 'dart:math';
 
 //Beinhaltet:
 //QUWERTZU-Layout für die Enigma1 und Naval Enigma
-// 2 Buchstaben haben jeweils eine Farbe 
+// 2 Buchstaben haben jeweils eine Farbe
 // Begrenzung auf 20 Paare + inklusive Fehler-Meldung
 // Fehler-Meldung, dass eine Verknüfung gewählt werden muss
 // Aufhebung der gewählten Buchstaben durch Backspace-Taste
 // Reset-Button für Werkseinstellungen
-
 
 void main() {
   runApp(MyApp());
@@ -23,10 +22,9 @@ class MyApp extends StatelessWidget {
       title: 'Enigma I Steckerbrett',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Enigma I: QWERTZU-Steckerbrett'
-        ),
+          title: const Text('Enigma I: QWERTZU-Steckerbrett'),
           centerTitle: true, // Titel zentrieren
-        ), 
+        ),
         body: CustomKeyboard(),
       ),
     );
@@ -42,7 +40,7 @@ class _CustomKeyboardState extends State<CustomKeyboard> {
   String _inputText = '';
   List<bool> _isButtonSelected = List.generate(26, (_) => false);
   int _selectedCount = 0;
-  List<Color> _availableColors = [
+  final List<Color> _availableColors = [
     Colors.red,
     Colors.blue,
     Colors.green,
@@ -56,7 +54,7 @@ class _CustomKeyboardState extends State<CustomKeyboard> {
   ];
 
   // Dictionary, um Buchstabenpaare und ihre Farben zu speichern
-  Map<String, Color> _letterColorMap = {};
+  final Map<String, Color> _letterColorMap = {};
 
   void _onKeyPressed(String value) {
     if (_selectedCount < 20) {
@@ -67,26 +65,59 @@ class _CustomKeyboardState extends State<CustomKeyboard> {
         _selectedCount++;
 
         if (_selectedCount % 2 == 0) {
-        // Wähle eine zufällige Farbe für das Buchstabenpaar, die noch nicht verwendet wurde
-        final availableColors = _availableColors.where((color) => !_letterColorMap.containsValue(color)).toList();
-        if (availableColors.isNotEmpty) {
-          final randomColor = availableColors[Random().nextInt(availableColors.length)];
-          _letterColorMap[value] = randomColor;
-          _letterColorMap[_inputText[_inputText.length - 2]] = randomColor;
+          // Wähle eine zufällige Farbe für das Buchstabenpaar, die noch nicht verwendet wurde
+          final availableColors = _availableColors
+              .where((color) => !_letterColorMap.containsValue(color))
+              .toList();
+
+          if (availableColors.isNotEmpty) {
+            final randomColor =
+                availableColors[Random().nextInt(availableColors.length)];
+            _letterColorMap[value] = randomColor;
+            _letterColorMap[_inputText[_inputText.length - 2]] = randomColor;
+          } else {
+            _showSnackbar("A selection error has occurred!", Colors.red);
+          }
         }
-      }
       });
+    } else {
+      _showSnackbar(
+          'Maximale Anzahl an wählbaren Verbindungen erreicht!', Colors.red);
     }
   }
 
-  void _onDeletePressed() {
+  void _onDeletePressed(String value) {
     setState(() {
-      if (_inputText.isNotEmpty) {
-        final lastChar = _inputText.substring(_inputText.length - 1);
-        final charIndex = lastChar.codeUnitAt(0) - 65;
-        _inputText = _inputText.substring(0, _inputText.length - 1);
-        _isButtonSelected[charIndex] = false;
-        _selectedCount--;
+      // check for selected button -> easy fix
+      final charIndex = value.codeUnitAt(0) - 65;
+      if (_isButtonSelected[charIndex] && _selectedCount % 2 == 1) {
+        // if is self, then unselect
+        if (!_letterColorMap.containsKey(value)) {
+          _isButtonSelected[charIndex] = false;
+          _selectedCount--;
+          _inputText = _inputText.replaceAll(value, '');
+        } else {
+          _showSnackbar("This plug is already in use, select another one!",
+              Colors.redAccent);
+        }
+        return;
+      }
+
+      if (_selectedCount > 0) {
+        // get both keys with the same color
+        List<String> keys = _letterColorMap.keys
+            .where((key) => _letterColorMap[key] == _letterColorMap[value])
+            .toList();
+
+        for (String key in keys) {
+          final charIndex = key.codeUnitAt(0) - 65;
+
+          _inputText = _inputText.replaceAll(key, '');
+          _letterColorMap.remove(key);
+
+          _isButtonSelected[charIndex] = false;
+          _selectedCount--;
+        }
       }
     });
   }
@@ -95,7 +126,7 @@ class _CustomKeyboardState extends State<CustomKeyboard> {
     setState(() {
       // Lösche die aktuellen Farben
       _letterColorMap.clear();
-      
+
       _inputText = '';
       _isButtonSelected = List.generate(26, (_) => false);
       _selectedCount = 0;
@@ -103,41 +134,39 @@ class _CustomKeyboardState extends State<CustomKeyboard> {
   }
 
   void _showSnackbar(String message, Color color) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(message),
-        backgroundColor: color,
-      ),
+          content: Text(message),
+          backgroundColor: color,
+          duration: const Duration(seconds: 3)),
     );
   }
 
   Widget _buildKeyboardButton(String value) {
     final isSelected = _isButtonSelected[value.codeUnitAt(0) - 65];
-    final letterColor = _letterColorMap[value] ?? const Color.fromARGB(255, 134, 182, 136); // Standardfarbe
-    
+    final letterColor = _letterColorMap[value] ??
+        const Color.fromARGB(255, 134, 182, 136); // Standardfarbe
+
     return ElevatedButton(
       onPressed: () {
         if (isSelected) {
-          _onDeletePressed();
+          _onDeletePressed(value);
         } else {
           _onKeyPressed(value);
-        }
-
-        //Fehler-Meldungen
-        if (_selectedCount == 20) {
-          _showSnackbar('Maximale Anzahl an wählbaren Verbindungen erreicht!', Colors.red);
-        } else if (_selectedCount == 1) {
-          _showSnackbar('Wähle mindestens zwei Buchstaben!', Colors.red);
         }
       },
       style: ElevatedButton.styleFrom(
         fixedSize: const Size(60, 60),
         shape: const CircleBorder(),
-        backgroundColor: isSelected ? letterColor : const Color.fromARGB(255, 34, 34, 34).withOpacity(0.1),
+        backgroundColor: isSelected
+            ? letterColor
+            : const Color.fromARGB(255, 34, 34, 34).withOpacity(0.1),
       ),
       child: Text(
         value,
-        style: TextStyle(color: Color.fromARGB(247, 255, 255, 255), fontSize: 18),
+        style:
+            TextStyle(color: Color.fromARGB(247, 255, 255, 255), fontSize: 18),
       ),
     );
   }
@@ -197,11 +226,11 @@ class _CustomKeyboardState extends State<CustomKeyboard> {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            IconButton(
-              padding: EdgeInsets.all(0),
-              icon: Icon(Icons.backspace),
-              onPressed: _onDeletePressed,
-            ),
+            // IconButton(
+            //   padding: EdgeInsets.all(0),
+            //   icon: Icon(Icons.backspace),
+            //   onPressed: _onDeletePressed,
+            // ),
             ElevatedButton(
               onPressed: _resetKeyboard,
               child: Text('Reset'),
