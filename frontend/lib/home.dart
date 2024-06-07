cff2921afa12bc6cbc1dbd5b4dd43a380139e2a1
ts19084 <thomas.schauer-koeckeis@stud.th-deg.de>
2024-06-07 22:17:20 +0200

    Revert "GUI in Homepage"
    This reverts commit 25a7467cd65488c28b7943b1f7bbe1c883f8fec6.

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:enigma/lampenfeld.dart';
 import 'package:enigma/sidebar.dart';
 import 'package:enigma/utils.dart';
-import 'walzen.dart'; // Import der Rotoren-Seite
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -68,38 +67,7 @@ class HomePageState extends State<HomePage> {
         onItemSelected: updateSelectedItem,
         key: const Key('enigma_sidebar'),
       ),
-      body: Column(
-        children: <Widget>[
-          const Expanded(
-            child: Center(
-              child: Lampfield(),
-            ),
-          ),
-          SizedBox(height: 20), // Abstand zwischen Lampfield und Rotoren
-          Container(
-            alignment: Alignment.bottomCenter,
-            padding: EdgeInsets.symmetric(vertical: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    // Implementiere die Logik vom Startbutton hier
-                  },
-                  child: const Text('Start'),
-                ),
-                SizedBox(width: 10), // Abstand zwischen Start-Button und erstem Rotor
-                for (int i = 0; i < 5; i++)
-                  SizedBox(
-                    width: 150, // Breite der Rotoren-Widgets 
-                    child: RotorWidget(rotorNumber: i + 1),
-                  ),
-              ],
-            ),
-          ),
-          SizedBox(height: 20), // Abstand zwischen Rotoren und Ende der Seite
-        ],
-      ),
+      body: const Lampfield(),
     );
   }
 }
