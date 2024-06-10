536f1a06b0886c7bea03477cc8b3e3db0c16279b
David Horvát <horvatda@proton.me>
2024-06-10 21:46:14 +0200

    applied fixes

--- frontend/lib/sidebar.dart
+++ frontend/lib/sidebar.dart
@@ -26,9 +26,9 @@ class SideBar extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: <Widget>[
           header,
-          Machine(name: 'Enigma I', id: "", onItemSelected: this.onItemSelected),
-          Machine(name: 'Norway Enigma', id: "", onItemSelected: this.onItemSelected),
-          Machine(name: 'Enigma M3', id: "", onItemSelected: this.onItemSelected),
+          Machine(name: 'Enigma I', id: "", onItemSelected: onItemSelected),
+          Machine(name: 'Norway Enigma', id: "", onItemSelected: onItemSelected),
+          Machine(name: 'Enigma M3', id: "", onItemSelected: onItemSelected),
           addMachine(context)
         ],
       ),
