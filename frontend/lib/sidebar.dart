a8d3a75a9b28f7571679b60b47d10dd73c3aed12
Henjujo <henjugames@gmail.com>
2024-06-24 03:05:00 +0200

    Quick change

--- frontend/lib/sidebar.dart
+++ frontend/lib/sidebar.dart
@@ -1,7 +1,7 @@
-import 'package:enigma/new_machine.dart';
+import 'package:enigma/custom_machine.dart';
 import 'package:flutter/material.dart';
 import 'package:enigma/utils.dart';
-import 'new_machine.dart';
+import 'custom_machine.dart';
 
 class SideBar extends StatelessWidget {
   final String username;
