828813cea32f69a21c685552041612479e930a7b
Sam <samuel.backof@stud.th-deg.de>
2024-06-27 17:21:52 +0200

    Added test template

---
+++ frontend/test_driver/rotor_test.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+import 'test_lib.dart';
+
+void main() {
+  FlutterDriver? driver;
+
+  setUp(() async {
+    driver = await FlutterDriver.connect();
+    driver?.checkHealth();
+  });
+
+  tearDown(() async {
+    if (driver != null) {
+      driver?.close();
+    }
+  });
+
+  //tests
+
+}
