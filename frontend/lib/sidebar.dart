62c2ad79cd5dad7ecd10120270b578ed2a0ae979
Henjujo <henjugames@gmail.com>
2024-06-27 22:13:22 +0200

    Adding machines works on frontend

--- frontend/lib/sidebar.dart
+++ frontend/lib/sidebar.dart
@@ -7,27 +7,8 @@ import 'dart:convert';
 class SideBar extends StatelessWidget {
   final String username;
 
-  SideBar({super.key, required this.username});
+  const SideBar({super.key, required this.username});
 
-  String? _selectedValuePlugboardToggle;
-  String? _selectedValueRotorenAnzahl;
-  String? _selectedValueRotorenAuswahl;
-  String? _selectedValueUmkehrwalzen;
-
-  List<String> itemsRotorenAuswahl = ["1", "2", "3", "4", "5"];
-  List<String> itemsUmkehrwalzen = ["A", "B", "C", "D", "E"];
-
-  bool enableButton() {
-    if (_selectedValueRotorenAnzahl == null) {
-      return false;
-    }
-    return _selectedValuePlugboardToggle != null &&
-        int.tryParse(_selectedValueRotorenAnzahl!) != 0 &&
-        int.tryParse(_selectedValueRotorenAnzahl!) !=
-            null && //Extra check for empty string
-        _selectedValueRotorenAuswahl != null &&
-        _selectedValueUmkehrwalzen != null;
-  }
 
   Widget getHeader() {
     return DrawerHeader(
@@ -67,131 +48,7 @@ class SideBar extends StatelessWidget {
         showDialog(
           context: context,
           builder: (BuildContext context) {
-            return FutureBuilder<List<dynamic>>(
-              future: Future.wait([getRotorIDs(), getUmkehrwalzenIDs()]),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return AlertDialog(
-                    title: const Text("Error"),
-                    content: Text("Error: ${snapshot.error}"),
-                    actions: <Widget>[
-                      TextButton(
-                        child: const Text("Close"),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  );
-                } else {
-                  var itemsRotorenAuswahl = snapshot.data![0] as List<String>;
-                  var itemsUmkehrwalzen = snapshot.data![1] as List<String>;
-
-                  return AlertDialog(
-                    title: const Text("Neue Maschine"),
-                    content: SizedBox(
-                      height: 200,
-                      width: 800,
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            // Row with Drop-down menus
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  children: [
-                                    const Text("Plugboard erlauben?"),
-                                    StatefulDropdownMenu(
-                                      selectedValue: _selectedValuePlugboardToggle,
-                                      onChanged: (value) {
-                                        _selectedValuePlugboardToggle = value;
-                                      },
-                                      items: const ["Ja", "Nein"],
-                                    ),
-                                  ],
-                                ),
-                                Column(
-                                  children: [
-                                    const Text("Anzahl an Rotoren"),
-                                    SizedBox(
-                                      width: 150.0,
-                                      height: 50.0,
-                                      child: TextField(
-                                        onChanged: (String value) {
-                                          _selectedValueRotorenAnzahl = value;
-                                        },
-                                        inputFormatters: [
-                                          FilteringTextInputFormatter.allow(
-                                              RegExp(r'[0-9]')),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Column(
-                                  children: [
-                                    const Text("Auswählbare Rotoren"),
-                                    StatefulDropdownMenu(
-                                      selectedValue: _selectedValueRotorenAuswahl,
-                                      onChanged: (value) {
-                                        _selectedValueRotorenAuswahl = value;
-                                      },
-                                      items: itemsRotorenAuswahl,
-                                    ),
-                                  ],
-                                ),
-                                Column(
-                                  children: [
-                                    const Text("Auswählbare Umkehrwalzen"),
-                                    StatefulDropdownMenu(
-                                      selectedValue: _selectedValueUmkehrwalzen,
-                                      onChanged: (value) {
-                                        _selectedValueUmkehrwalzen = value;
-                                      },
-                                      items: itemsUmkehrwalzen,
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 20),
-                            // Buttons at the bottom right
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const Text("Abbrechen"),
-                                ),
-                                const SizedBox(width: 8),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    if(enableButton()) {
-                                      _selectedValuePlugboardToggle = null;
-                                      _selectedValueRotorenAnzahl = null;
-                                      _selectedValueRotorenAuswahl = null;
-                                      _selectedValueUmkehrwalzen = null;
-                                      Navigator.of(context).pop();
-                                    }
-                                  },
-                                  child: const Text("Maschine erstellen"),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                }
-              },
-            );
+            return AddMachinePopUp();
           },
         );
       },
@@ -222,6 +79,171 @@ class SideBar extends StatelessWidget {
     );
   }
 }
+
+class AddMachinePopUp extends StatefulWidget{
+
+  @override
+  AddMachinePopUpState createState() => AddMachinePopUpState();
+}
+
+class AddMachinePopUpState extends State<AddMachinePopUp>{
+
+  String? _selectedValuePlugboardToggle;
+  String? _selectedValueRotorenAnzahl;
+  List<String> _selectedValueRotorenAuswahl = [];
+  List<String> _selectedValueUmkehrwalzen = [];
+
+  List<String> itemsRotorenAuswahl = [];
+  List<String> itemsUmkehrwalzen = [];
+
+  bool enableButton() {
+    if (_selectedValueRotorenAnzahl == null) {
+      return false;
+    }
+    return _selectedValuePlugboardToggle != null &&
+        int.tryParse(_selectedValueRotorenAnzahl!) != 0 &&
+        int.tryParse(_selectedValueRotorenAnzahl!) !=
+            null && //Extra check for empty string
+        _selectedValueRotorenAuswahl.isNotEmpty &&
+        _selectedValueUmkehrwalzen.isNotEmpty;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<dynamic>>(
+      future: Future.wait([getRotorIDs(), getUmkehrwalzenIDs()]),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return AlertDialog(
+            title: const Text("Error"),
+            content: Text("Error: ${snapshot.error}"),
+            actions: <Widget>[
+              TextButton(
+                child: const Text("Close"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        } else {
+          var itemsRotorenAuswahl = snapshot.data![0] as List<String>;
+          var itemsUmkehrwalzen = snapshot.data![1] as List<String>;
+
+          return AlertDialog(
+            title: const Text("Neue Maschine"),
+            content: SizedBox(
+              height: 200,
+              width: 800,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    // Row with Drop-down menus
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            const Text("Plugboard erlauben?"),
+                            StatefulDropdownMenu(
+                              selectedValue: _selectedValuePlugboardToggle,
+                              onChanged: (value) {
+                                _selectedValuePlugboardToggle = value;
+                              },
+                              items: const ["Ja", "Nein"],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            const Text("Anzahl an Rotoren"),
+                            SizedBox(
+                              width: 150.0,
+                              height: 50.0,
+                              child: TextField(
+                                onChanged: (String value) {
+                                  _selectedValueRotorenAnzahl = value;
+                                },
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.allow(
+                                      RegExp(r'[0-9]')),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            //const Text("Auswählbare Rotoren"),
+                            StatefulCheckboxMenu(
+                              selectedValues: _selectedValueRotorenAuswahl,
+                              onChanged: (value) {
+                                _selectedValueRotorenAuswahl = value;
+                              },
+                              items: itemsRotorenAuswahl,
+                              name: "Rotor(en) auswählen"
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            StatefulCheckboxMenu(
+                              selectedValues: _selectedValueUmkehrwalzen,
+                              onChanged: (value) {
+                                _selectedValueUmkehrwalzen = value;
+                              },
+                              items: itemsUmkehrwalzen,
+                              name: "Umkehrwalze(n) auswählen",
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    // Buttons at the bottom right
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text("Abbrechen"),
+                        ),
+                        const SizedBox(width: 8),
+                        ElevatedButton(
+                          onPressed: () {
+                            if(enableButton()) {
+                              //print("To print: $_selectedValuePlugboardToggle, $_selectedValueRotorenAnzahl, $_selectedValueRotorenAuswahl, $_selectedValueUmkehrwalzen");
+                              //Can be used for debugging
+                              _selectedValuePlugboardToggle = null;
+                              _selectedValueRotorenAnzahl = null;
+                              _selectedValueRotorenAuswahl = [];
+                              _selectedValueUmkehrwalzen = [];
+                              Navigator.of(context).pop();
+                              // Add backend call here!
+                              // Add new machine to sidebar as well!
+                            }
+                          },
+                          child: const Text("Maschine erstellen"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
 
 class StatefulDropdownMenu extends StatefulWidget {
   final String? selectedValue;
@@ -265,6 +287,93 @@ class StatefulDropdownMenuState extends State<StatefulDropdownMenu> {
         });
         widget.onChanged(value);
       },
+    );
+  }
+}
+
+class StatefulCheckboxMenu extends StatefulWidget {
+  final List<String> selectedValues;
+  final ValueChanged<List<String>> onChanged;
+  final List<String> items;
+  final String name;
+
+  const StatefulCheckboxMenu({
+    super.key,
+    required this.selectedValues,
+    required this.onChanged,
+    required this.items,
+    required this.name,
+  });
+
+  @override
+  StatefulCheckboxMenuState createState() => StatefulCheckboxMenuState();
+}
+
+class StatefulCheckboxMenuState extends State<StatefulCheckboxMenu> {
+  List<String> _selectedValues = [];
+  late String name;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedValues = widget.selectedValues;
+    name = widget.name;
+  }
+
+  void _onItemCheckedChange(String itemValue, bool isChecked) {
+    setState(() {
+      if (isChecked) {
+        _selectedValues.add(itemValue);
+      } else {
+        _selectedValues.remove(itemValue);
+      }
+      widget.onChanged(_selectedValues);
+    });
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<String>(
+      child: Row(
+        children: [
+          Text(name),
+          const Icon(Icons.arrow_drop_down),
+        ],
+      ),
+      itemBuilder: (BuildContext context) {
+        return [
+          PopupMenuItem<String>(
+            enabled: false,
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxHeight: 400,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: widget.items.map((String item) {
+                    return StatefulBuilder(
+                      builder: (BuildContext context, StateSetter setState) {
+                        final isChecked = _selectedValues.contains(item);
+                        return CheckboxListTile(
+                          title: Text(item),
+                          value: isChecked,
+                          onChanged: (bool? checked) {
+                            setState(() {
+                              _onItemCheckedChange(item, checked!);
+                            });
+                          },
+                        );
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ),
+        ];
+      },
+      onSelected: (_) {},
     );
   }
 }
