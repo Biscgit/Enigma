019960a1b409ec2a51be01cd9272466856287936
Henjujo <henjugames2@gmail.com>
2024-06-29 11:59:57 +0200

    Add CreateMachine to testlib

--- frontend/test_driver/test_lib.dart
+++ frontend/test_driver/test_lib.dart
@@ -61,3 +61,57 @@ Future<void> writeChar(String char, FlutterDriver? driver) async {
   assert(char.length == 1);
   await driver?.tap(find.byValueKey("Tastatur-Button-${char.toUpperCase()}"));
 }
+
+Future<void> createMachine(FlutterDriver? driver, String name, bool plugboardOn, int rotorCount, List<int> rotorList, List<int> uKWList) async {
+  //rotorCount > 0; rotorList only contains elements in [1; 18]
+  
+  if(rotorCount <= 0) {
+    return;
+  }
+  
+  final addButton = find.byValueKey('addButton');
+  await driver?.tap(addButton);
+
+  final nameTextfield = find.byValueKey("AddMachine-Key-Name");
+  await driver?.waitFor(nameTextfield, timeout: const Duration(seconds: 10));
+  await driver?.tap(nameTextfield);
+  await driver?.enterText(name);
+
+  var plugboardSwitch = find.byValueKey("AddMachine-Key-Plugboard");
+  await driver?.tap(plugboardSwitch);
+  await driver?.tap(plugboardSwitch);
+  await driver?.tap(plugboardSwitch); //On, off, on
+  if(!plugboardOn) {
+    await driver?.tap(plugboardSwitch); //off again
+  }
+  
+  final rotorTextfield = find.byValueKey("AddMachine-Key-RotorNr");
+  await driver?.tap(rotorTextfield);
+  await driver?.enterText(rotorCount.toString());
+
+  for(int i in rotorList) {
+    await driver?.tap(find.byValueKey("Checkbox-Key-Rotor $i"));
+  }
+
+  for(int x in uKWList) {
+    String ukwKey = "";
+    if(x == 0) {
+      ukwKey = "-A";
+    }
+    else if(x == 1) {
+      ukwKey = "-B";
+    }
+    else if(x == 2) {
+      ukwKey = "-C";
+    }
+    else if(x == 3) {
+      ukwKey = "-D*";
+    }
+    /*
+    else if(x == 4) {
+      ukwKey = "";
+    }
+    */
+    await driver?.tap(find.byValueKey("Checkbox-Key-UKW$ukwKey"));
+  }
+}
