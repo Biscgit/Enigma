bfcde01966d540565d6309e5251d015622d71b6d
David Horvát <horvatda@proton.me>
2024-06-30 23:34:38 +0200

    moved plugboard test down because of relying on adding machines

--- frontend/test_driver/app_test.dart
+++ frontend/test_driver/app_test.dart
@@ -18,11 +18,11 @@ void main() {
     keyhistory_test.main();
   });
 
-  group('Plugboard:', () {
-    plugboard_test.main();
-  });
-
   group('Adding machines:', () {
     add_machine_test.main();
+  });
+
+  group('Plugboard:', () {
+    plugboard_test.main();
   });
 }
